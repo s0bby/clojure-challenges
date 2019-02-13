a0bec3890b329fb08585f79820d5bab1d22feb3d
bugrimov <bugrimov@cma.ru>
2019-02-13 12:47:53 +0300

    3 printing quotes

---
+++ 3-printing-quotes/src/printing_quotes/core.clj
@@ -0,0 +1,6 @@
+(ns printing-quotes.core
+  (:gen-class))
+
+(defn -main
+  [& args]
+  (println "What is the quote? These aren't the droids you're looking for.\nWho said it? Obi-Wan Kenobi \nObi-Wan Kenobi says, \"These aren't the droids you're looking for.\""))
